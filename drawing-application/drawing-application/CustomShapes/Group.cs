195c72e2cf533d1f4254c3a24d8684c91424c69f
merlijn <mn.stokhorst@gmail.com>
2020-03-09 21:50:16 +0100

    added recursive method the get every shape in a group and their subgroup

--- drawing-application/drawing-application/CustomShapes/Group.cs
+++ drawing-application/drawing-application/CustomShapes/Group.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
+using System.Linq;
 
 namespace drawing_application.CustomShapes
 {
@@ -14,6 +15,24 @@ namespace drawing_application.CustomShapes
         public void AddChild(CustomShape shape)
         {
             childeren.Add(shape);
+        }
+
+        public List<CustomShape> GetAllShapes()
+        {
+            var shapes = childeren.Where(T => T.GetType() != typeof(Group)).ToList();
+            var groups = childeren.Where(T => T.GetType() == typeof(Group)).ToList();
+
+            foreach (var group in groups)
+            {
+                shapes.AddRange(((Group)group).GetAllShapes());
+            }
+
+            return shapes;
+        }
+
+        public void RemoveChild(CustomShape shape)
+        {
+            childeren.Remove(shape);
         }
 
         public override void Move(Point offset)
